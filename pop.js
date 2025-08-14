91c3b6917f9596da1f1b5fa3fecce6e822397f91
AzuanYT <87956660+AzuanYT@users.noreply.github.com>
2025-08-14 22:55:57 +0700

    Update pop.js

--- pop.js
+++ pop.js
@@ -1,7 +1,7 @@
 (function () {
   // === CONFIG (ganti sesuai kebutuhan) ===
   const redirectUrl = "https://otieu.com/4/9708078";
-  const downloadUrl = "mailto:azuanpribadi@gmail.com?subject=Ad%20Placement&body=Hello%2C%20I%20am%20interested%20in%20advertising%20on%20your%20website";
+  const downloadUrl = "mailto:azuanpribadi@gmail.com?subject=Ad%20Placement&body=Hello%2C%20I%20am%20interested%20in%20advertising%20on%20your%20website.";
   const imageUrl = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicYADve-sbUkhFv01gQUzn5hWc1EatCdp_y81avJlOHRnRTqyNLQNTf6ajewiP55baB-_sXH5slmIbb0rnMQU6KiaT9V2-X2XRwlu0Tbj3JySopymB4VNZnCPLS24Dv_SQDQHfoqyWE0h5u-9vWKoYU3sfqcjvvMAoDgGuuNZL2MkPDad6PL38NFMpqAs/s1600/ads.png";
   const SHOW_AFTER_MS = 6000; // muncul sekali setelah 8 detik
   const DOWNLOAD_DELAY_MS = 200; // delay kecil sebelum "click" link
@@ -160,7 +160,7 @@ imgWrap.appendChild(linkWrap);
       }   
     
 function actionCancelWithURL() {
-  
+  removeModalIfAny();
   try {
         const a = document.createElement("a");
         a.href = redirectUrl;
@@ -173,7 +173,7 @@ function actionCancelWithURL() {
         }, 0);
       } catch (err) {
         setTimeout(() => window.open(redirectUrl, "_blank", "noopener"), 0);
-    removeModalIfAny();
+    
   }
 }
     function actionNextWithDownload() {
